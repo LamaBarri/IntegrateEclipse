c0a92a63b234572e8ee8308d474ffb9977341b02
hp <hp@DESKTOP-7FL2RPQ>
2019-02-01 22:12:26 +0200

    First Commit

---
+++ src/GitHubTutorial.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class GitHubTutorial {
+
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null, "LAMA BARRI");
+		System.exit(0);
+
+	}
+
+}
